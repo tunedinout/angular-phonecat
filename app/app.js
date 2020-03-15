59af72e092639bb97401cc4f1ca936956c442c55
tunedinout <narottam2911@gmail.com>
2020-03-15 23:07:57 +0530

    Step 2 complete

--- app/app.js
+++ app/app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+var phonecatApp = angular.module('phonecatApp',[]);
+
+phonecatApp.controller('PhoneListController', function PhoneListController($scope){
+  $scope.phones = [
+    {
+      name : 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S'
+    },
+    {
+      name : 'Motorola XOOMᵀᴹ with wifi',
+      snippet: 'The Next, Next generation tablet.'
+    },
+    {
+      name : 'Motorola XOOMᵀᴹ',
+      snippet: 'The Next, Next generation tablet.'
+    }
+  ];
+})
